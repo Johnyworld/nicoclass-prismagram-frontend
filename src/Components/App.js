47954800bd3fe37a53cfb5e4f09842fd39ef7a2b
Johny Kim <johnyworld@naver.com>
2019-08-16 13:43:31 +0900

    Set : Apollo settings done

---
+++ src/Components/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { gql } from 'apollo-boost';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyles from '../Styles/GlobalStyles';
+import Theme from '../Styles/Theme';
+import AppRouter from './Router';
+import { useQuery } from 'react-apollo-hooks';
+
+const QUERY = gql`
+  {isLoggedIn @client}
+`
+
+export default () => {
+  const { data: {isLoggedIn} } = useQuery(QUERY);
+
+  return (
+    <ThemeProvider theme={Theme}>
+      <>
+        <GlobalStyles />
+        <AppRouter isLoggedIn={isLoggedIn} />
+      </>
+    </ThemeProvider>
+  )
+}
